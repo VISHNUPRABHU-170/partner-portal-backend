f35a8d379a42d5e58cf2d1e456ea1df4096ab631
VISHNUPRABHU-170 <vishnurr1708@gmail.com>
2024-12-14 23:37:25 +0530

    Implemented Auth Routes, Auth Controller, and Auth Service.

---
+++ app.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import cors from 'cors';
+import mainRoutes from './routes/mainRoutes.js';
+import connectDB from './db.js';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+connectDB();
+
+app.use("/api/partner-portal", mainRoutes);
+
+export default app;
